b62769476100b42d2ca7bd1601c9154b7fb57620
michal_jaworski <michal.jaworski@students.mimuw.edu.pl>
2018-08-15 14:45:25 +0200

    15:45

--- Projekt.py
+++ Projekt.py
@@ -3,6 +3,9 @@ import math
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import preprocessing
+from scipy.io import savemat
+from mpl_toolkits import mplot3d
 from sklearn.metrics import classification_report
 
 COLUMN_NAMES = ['T_xacc', 'T_yacc', 'T_zacc', 'T_xgyro', 'T_ygyro', 'T_zgyro', 'T_xmag', 'T_ymag', 'T_zmag',
@@ -102,3 +105,75 @@ df_train['Segment'][10]
 
 plt.scatter(result['distance'], result['feature'])  # arguments are passed to np.histogram
 plt.show()
+
+# three dimension
+df_train = df[['T_xacc', 'T_yacc', 'T_zacc', 'Action', 'Subject', 'Segment']]
+
+def distance_three(df):
+    origin = df[['T_xacc', 'T_yacc', 'T_zacc']][df['Action'] == 1][df['Subject'] == 1][df['Segment'] == 1]
+    distance = pd.DataFrame(index=list(range(0, 1140)), columns=['observation', 'distance_x', 'distance_y', 'distance_z'])
+    for i in range(1, 20):
+        for j in range(1, 61):
+            test = df['T_xacc'][df['Action'] == i][df['Subject'] == 1][df['Segment'] == j]
+            distance['distance_x'][(i - 1) * 60 + j - 1] = DTWDistance(origin['T_xacc'].values, test.values)
+            test = df['T_yacc'][df['Action'] == i][df['Subject'] == 1][df['Segment'] == j]
+            distance['distance_y'][(i - 1) * 60 + j - 1] = DTWDistance(origin['T_yacc'].values, test.values)
+            test = df['T_zacc'][df['Action'] == i][df['Subject'] == 1][df['Segment'] == j]
+            distance['distance_z'][(i - 1) * 60 + j - 1] = DTWDistance(origin['T_zacc'].values, test.values)
+            distance['observation'][(i - 1) * 60 + j - 1] = i
+    return distance
+
+result = distance_three(df_train)
+ax = plt.axes(projection='3d')
+colors = result['observation']
+ax.scatter(result['distance_x'], result['distance_y'], result['distance_z'], c=colors)
+
+# NORMALISED
+min_max_scaler = preprocessing.MinMaxScaler()
+np_scaled = min_max_scaler.fit_transform(result[['distance_x', 'distance_y', 'distance_z']])
+result_normalised = pd.DataFrame(np_scaled)
+result_normalised['observation'] = result['observation']
+ax = plt.axes(projection='3d')
+colors = result_normalised['observation']
+ax.scatter(result_normalised[0], result_normalised[1], result_normalised[2], c=colors)
+
+result_normalised.columns = ['x', 'y', 'z', 'observation']
+savemat('result_normalised.mat', result_normalised)
+
+pd.to_csv(result_normalised)
+
+# Train test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(result_normalised[[0, 1, 2]], result_normalised['observation'], test_size=0.2, random_state=42)
+
+# KNeighbors
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=5)
+neigh.fit(X_train, list(y_train))
+neigh.predict(X_test)
+neigh.score(X_test,list(y_test))
+list(y_test) - neigh.predict(X_test)
+# Parameter tuning
+from sklearn.neighbors import KNeighborsClassifier
+from skopt.space import Real, Integer
+from skopt.utils import use_named_args
+
+
+# The list of hyper-parameters we want to optimize. For each one we define the bounds,
+# the corresponding scikit-learn parameter name, as well as how to sample values
+# from that dimension (`'log-uniform'` for the learning rate)
+space  = [Integer(1, 5, name='n_'),
+          Real(10**-5, 10**0, "log-uniform", name='learning_rate'),
+          Integer(1, n_features, name='max_features'),
+          Integer(2, 100, name='min_samples_split'),
+          Integer(1, 100, name='min_samples_leaf')]
+
+# this decorator allows your objective function to receive a the parameters as
+# keyword arguments. This is particularly convenient when you want to set scikit-learn
+# estimator parameters
+@use_named_args(space)
+def objective(**params):
+    reg.set_params(**params)
+
+    return -np.mean(cross_val_score(reg, X, y, cv=5, n_jobs=-1,
+                                    scoring="neg_mean_absolute_error"))
