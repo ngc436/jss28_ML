50d00ba4f52350252f8ad60b5c9ce145add463f5
michal_jaworski <michal.jaworski@students.mimuw.edu.pl>
2018-08-15 21:08:29 +0200

    results_all 3d

--- Projekt.py
+++ Projekt.py
@@ -139,12 +139,14 @@ ax.scatter(result_normalised[0], result_normalised[1], result_normalised[2], c=c
 
 result_normalised.columns = ['x', 'y', 'z', 'observation']
 savemat('result_normalised.mat', result_normalised)
+savemat('result.mat', result)
 
 pd.to_csv(result_normalised)
 
 # Train test split
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(result_normalised[[0, 1, 2]], result_normalised['observation'], test_size=0.2, random_state=42)
+X_train.columns = ['x', 'y', 'z']
 
 # KNeighbors
 from sklearn.neighbors import KNeighborsClassifier
@@ -153,27 +155,205 @@ neigh.fit(X_train, list(y_train))
 neigh.predict(X_test)
 neigh.score(X_test,list(y_test))
 list(y_test) - neigh.predict(X_test)
+
 # Parameter tuning
 from sklearn.neighbors import KNeighborsClassifier
-from skopt.space import Real, Integer
+from skopt.space import Real, Integer, Categorical
 from skopt.utils import use_named_args
-
+from sklearn.model_selection import cross_val_score
 
 # The list of hyper-parameters we want to optimize. For each one we define the bounds,
 # the corresponding scikit-learn parameter name, as well as how to sample values
 # from that dimension (`'log-uniform'` for the learning rate)
-space  = [Integer(1, 5, name='n_'),
-          Real(10**-5, 10**0, "log-uniform", name='learning_rate'),
-          Integer(1, n_features, name='max_features'),
-          Integer(2, 100, name='min_samples_split'),
-          Integer(1, 100, name='min_samples_leaf')]
+space = [Integer(1, 100, name='n_neighbors'),
+         Categorical(['kd_tree', 'ball_tree', 'brute'], name='algorithm'),
+         Categorical(['uniform', 'distance'], name='weights'),
+         Integer(1, 3, name='p')]
 
 # this decorator allows your objective function to receive a the parameters as
 # keyword arguments. This is particularly convenient when you want to set scikit-learn
 # estimator parameters
 @use_named_args(space)
 def objective(**params):
+    reg = KNeighborsClassifier()
     reg.set_params(**params)
 
-    return -np.mean(cross_val_score(reg, X, y, cv=5, n_jobs=-1,
+    return -np.mean(cross_val_score(reg, X_train[['x', 'y', 'z']], list(y_train), cv=5, n_jobs=-1,
                                     scoring="neg_mean_absolute_error"))
+
+# Optimize
+from skopt import gp_minimize
+res_gp = gp_minimize(objective, space, n_calls=100, random_state=0)
+
+"Best score=%.4f" % res_gp.fun
+
+print("""Best parameters:
+- max_depth=%d
+- learning_rate=%.6f
+- max_features=%d
+- min_samples_split=%d
+- min_samples_leaf=%d""" % (res_gp.x[0], res_gp.x[1],
+                            res_gp.x[2], res_gp.x[3],
+                            res_gp.x[4]))
+
+# PLot of convergence
+from skopt.plots import plot_convergence
+plot_convergence(res_gp)
+
+########
+neigh = KNeighborsClassifier(n_neighbors=9, algorithm='kd_tree', weights='distance', p=1)
+neigh.fit(X_train, list(y_train))
+neigh.predict(X_test)
+neigh.score(X_test,list(y_test))
+# Score 0.8991228
+
+################################3 Random Forest
+from sklearn.ensemble import RandomForestClassifier
+space = [Integer(1, 100, name='n_estimators'),
+         Categorical(['gini', 'entropy'], name='criterion'),
+         Integer(1, 100, name='max_depth'),
+         Integer(2, 32, name='min_samples_split'),
+         Integer(1, 4, name='min_samples_leaf'),
+         Categorical(['False', 'True'], name='bootstrap')]
+
+# this decorator allows your objective function to receive a the parameters as
+# keyword arguments. This is particularly convenient when you want to set scikit-learn
+# estimator parameters
+@use_named_args(space)
+def objective(**params):
+    reg = RandomForestClassifier()
+    reg.set_params(**params)
+
+    return -np.mean(cross_val_score(reg, X_train[['x', 'y', 'z']], list(y_train), cv=5, n_jobs=-1,
+                                    scoring="neg_mean_absolute_error"))
+
+# Optimize
+from skopt import gp_minimize
+res_gp = gp_minimize(objective, space, n_calls=150, random_state=0)
+
+"Best score=%.4f" % res_gp.fun
+
+print("""Best parameters:
+- max_depth=%d
+- learning_rate=%.6f
+- max_features=%d
+- min_samples_split=%d
+- min_samples_leaf=%d""" % (res_gp.x[0], res_gp.x[1],
+                            res_gp.x[2], res_gp.x[3],
+                            res_gp.x[4]))
+
+# PLot of convergence
+from skopt.plots import plot_convergence
+plot_convergence(res_gp)
+
+###########
+rforest = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=69, min_samples_split=7, min_samples_leaf=1, bootstrap='True')
+rforest.fit(X_train, list(y_train))
+rforest.predict(X_test)
+rforest.score(X_test,list(y_test))
+
+
+
+
+################### FOR ALL DATA
+
+# three dimension
+df_train_all = df[['T_xacc', 'T_yacc', 'T_zacc', 'Action', 'Subject', 'Segment']]
+
+def distance_three(df):
+    origin = df[['T_xacc', 'T_yacc', 'T_zacc']][df['Action'] == 1][df['Subject'] == 1][df['Segment'] == 1]
+    distance = pd.DataFrame(index=list(range(0, 9120)), columns=['observation', 'distance_x', 'distance_y', 'distance_z'])
+    for i in range(1, 20):
+        for j in range(1, 61):
+            for k in range(1, 9):
+                test = df['T_xacc'][df['Action'] == i][df['Subject'] == k][df['Segment'] == j]
+                distance['distance_x'][(i - 1) * 480 + (j - 1)*8 + (k-1)] = DTWDistance(origin['T_xacc'].values, test.values)
+                test = df['T_yacc'][df['Action'] == i][df['Subject'] == k][df['Segment'] == j]
+                distance['distance_y'][(i - 1) * 480 + (j - 1)*8 + (k-1)] = DTWDistance(origin['T_yacc'].values, test.values)
+                test = df['T_zacc'][df['Action'] == i][df['Subject'] == k][df['Segment'] == j]
+                distance['distance_z'][(i - 1) * 480 + (j - 1)*8 + (k-1)] = DTWDistance(origin['T_zacc'].values, test.values)
+                distance['observation'][(i - 1) * 480 + (j - 1)*8 + (k-1)] = i
+    return distance
+
+result_all = distance_three(df_train_all)
+ax = plt.axes(projection='3d')
+colors = result_all['observation']
+ax.scatter(result_all['distance_x'], result_all['distance_y'], result_all['distance_z'], c=colors)
+
+# NORMALISED
+min_max_scaler = preprocessing.MinMaxScaler()
+np_scaled = min_max_scaler.fit_transform(result_all[['distance_x', 'distance_y', 'distance_z']])
+result_normalised_all = pd.DataFrame(np_scaled)
+result_normalised_all['observation'] = result_all['observation']
+ax = plt.axes(projection='3d')
+colors = result_normalised_all['observation']
+ax.scatter(result_normalised_all[0], result_normalised_all[1], result_normalised_all[2], c=colors)
+
+# result_all, result_normalised_all
+result_all.to_csv('data.csv')
+result_normalised_all.to_csv('data_normalised.csv')
+
+pd.save_to_csv('data', result)
+pd.save_to_csv('data', result_normalised)
+
+# Train test split for all
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(result_normalised_all[[0, 1, 2]], result_normalised_all['observation'], test_size=0.2, random_state=42)
+X_train.columns = ['x', 'y', 'z']
+
+# KNeighbors
+from sklearn.neighbors import KNeighborsClassifier
+neigh = KNeighborsClassifier(n_neighbors=5)
+neigh.fit(X_train, list(y_train))
+neigh.predict(X_test)
+neigh.score(X_test,list(y_test))
+list(y_test) - neigh.predict(X_test)
+
+# Parameter tuning
+from sklearn.neighbors import KNeighborsClassifier
+from skopt.space import Real, Integer, Categorical
+from skopt.utils import use_named_args
+from sklearn.model_selection import cross_val_score
+
+# The list of hyper-parameters we want to optimize. For each one we define the bounds,
+# the corresponding scikit-learn parameter name, as well as how to sample values
+# from that dimension (`'log-uniform'` for the learning rate)
+space = [Integer(1, 100, name='n_neighbors'),
+         Categorical(['kd_tree', 'ball_tree', 'brute'], name='algorithm'),
+         Categorical(['uniform', 'distance'], name='weights'),
+         Integer(1, 3, name='p')]
+
+# this decorator allows your objective function to receive a the parameters as
+# keyword arguments. This is particularly convenient when you want to set scikit-learn
+# estimator parameters
+@use_named_args(space)
+def objective(**params):
+    reg = KNeighborsClassifier()
+    reg.set_params(**params)
+
+    return -np.mean(cross_val_score(reg, X_train[['x', 'y', 'z']], list(y_train), cv=5, n_jobs=-1,
+                                    scoring="neg_mean_absolute_error"))
+
+# Optimize
+from skopt import gp_minimize
+res_gp = gp_minimize(objective, space, n_calls=100, random_state=0)
+
+"Best score=%.4f" % res_gp.fun
+
+print("""Best parameters:
+- max_depth=%d
+- learning_rate=%.6f
+- max_features=%d
+- min_samples_split=%d
+- min_samples_leaf=%d""" % (res_gp.x[0], res_gp.x[1],
+                            res_gp.x[2], res_gp.x[3],
+                            res_gp.x[4]))
+
+# PLot of convergence
+from skopt.plots import plot_convergence
+plot_convergence(res_gp)
+
+neigh = KNeighborsClassifier(n_neighbors=10, algorithm='ball_tree', weights='distance', p=1)
+neigh.fit(X_train, list(y_train))
+neigh.predict(X_test)
+neigh.score(X_test,list(y_test))
